0b669bb991ac7b0f2c4165953a936b4356d956f7
yy770986746-coder <yy770986746@gmail.com>
2026-07-21 05:01:58 +0800

    翻译UI为中文 - ProfileCreation + Devices

--- DevicesViewController.m
+++ DevicesViewController.m
@@ -1,1 +1,69 @@
-Sorry, too large.
+#import "DevicesViewController.h"
+#import "APIManager.h"
+
+@interface DevicesViewController ()
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray *devices;
+@end
+
+@implementation DevicesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"设备管理";
+    self.view.backgroundColor = [UIColor systemBackgroundColor];
+    
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.tableView];
+    
+    [self loadDevices];
+}
+
+- (void)loadDevices {
+    [[APIManager sharedManager] getUserDevices:^(NSArray *devices, NSInteger deviceLimit, NSError *error) {
+        if (!error) {
+            self.devices = devices;
+            [self.tableView reloadData];
+        }
+    }];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.devices.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DeviceCell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"DeviceCell"];
+    }
+    
+    NSDictionary *device = self.devices[indexPath.row];
+    cell.textLabel.text = device[@"name"] ?: @"未知设备";
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ - %@", device[@"model"] ?: @"", device[@"ios_version"] ?: @""];
+    
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSDictionary *device = self.devices[indexPath.row];
+        [[APIManager sharedManager] removeUserDevice:device[@"uuid"] completion:^(BOOL success, NSError *error) {
+            if (success) {
+                NSMutableArray *mutableDevices = [self.devices mutableCopy];
+                [mutableDevices removeObjectAtIndex:indexPath.row];
+                self.devices = mutableDevices;
+                [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            }
+        }];
+    }
+}
+
+@end
--- ProfileCreationViewController.m
+++ ProfileCreationViewController.m
@@ -1,1 +1,51 @@
-Sorry, too large.
+#import "ProfileCreationViewController.h"
+#import "ProfileManager.h"
+#import "DeviceModelManager.h"
+
+@interface ProfileCreationViewController ()
+@property (nonatomic, strong) UITextField *nameField;
+@property (nonatomic, strong) UITextField *deviceModelField;
+@property (nonatomic, strong) UIButton *createButton;
+@end
+
+@implementation ProfileCreationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"创建配置";
+    self.view.backgroundColor = [UIColor systemBackgroundColor];
+    
+    self.nameField = [[UITextField alloc] init];
+    self.nameField.placeholder = @"配置名称";
+    self.nameField.borderStyle = UITextBorderStyleRoundedRect;
+    self.nameField.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.nameField];
+    
+    self.deviceModelField = [[UITextField alloc] init];
+    self.deviceModelField.placeholder = @"设备型号";
+    self.deviceModelField.borderStyle = UITextBorderStyleRoundedRect;
+    self.deviceModelField.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.deviceModelField];
+    
+    self.createButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.createButton setTitle:@"创建" forState:UIControlStateNormal];
+    self.createButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.createButton];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [self.nameField.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:20],
+        [self.nameField.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20],
+        [self.nameField.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20],
+        [self.nameField.heightAnchor constraintEqualToConstant:44],
+        [self.deviceModelField.topAnchor constraintEqualToAnchor:self.nameField.bottomAnchor constant:20],
+        [self.deviceModelField.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:20],
+        [self.deviceModelField.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-20],
+        [self.deviceModelField.heightAnchor constraintEqualToConstant:44],
+        [self.createButton.topAnchor constraintEqualToAnchor:self.deviceModelField.bottomAnchor constant:30],
+        [self.createButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [self.createButton.widthAnchor constraintEqualToConstant:200],
+        [self.createButton.heightAnchor constraintEqualToConstant:44],
+    ]];
+}
+
+@end
